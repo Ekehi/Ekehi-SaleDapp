7e3985a0e9f5357881f1be6329c658dc189d91b6
Ekehi <ekehiofficial@gmail.com>
2024-02-19 11:40:59 -0800

    Updated menu

--- pages/homepageComponents/menu.js
+++ pages/homepageComponents/menu.js
@@ -31,7 +31,7 @@ export default function Menu()
                     <Navbar.Link
                         className="uppercase block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:hover:text-white text-gray-400 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
                         href="#section2">
-                        Section2
+                        Community
                     </Navbar.Link>
                     <Navbar.Link
                         className="uppercase block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:hover:text-white text-gray-400 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
