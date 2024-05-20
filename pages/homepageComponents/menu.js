432223b368ba9a15167fa288745c69e985fd0780
Ekehi <ekehiofficial@gmail.com>
2024-05-20 09:56:29 +0100

    fix

--- pages/homepageComponents/menu.js
+++ pages/homepageComponents/menu.js
@@ -45,7 +45,7 @@ export default function Menu()
                     </Navbar.Link>
                     <Navbar.Link
                         className="uppercasess block py-2 pr-4 pl-3 text-white rounded  default-txt md:p-0  hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-yellow-500"
-                        href="#">
+                        href="#presale">
                         Presales
                     </Navbar.Link>
                     <Navbar.Link
