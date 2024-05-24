7d6298713da559e43a280bd38ed6a692a10aeb4d
Ekehi <ekehiofficial@gmail.com>
2024-05-24 10:55:31 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,6 @@ export default function Home()
         <About />
         <Section3 />
         <Section4 />
-        <Airdrop />
         <Ecosystem />     
         <Tokenomics />
         <Roadmap />
