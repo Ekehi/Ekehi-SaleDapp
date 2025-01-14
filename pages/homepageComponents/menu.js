9dc6f05b37d4556b9f81f021eb9443eae96b36d2
Ekehi <160106638+Ekehi@users.noreply.github.com>
2025-01-14 18:25:59 +0000

    FRET

--- pages/homepageComponents/menu.js
+++ pages/homepageComponents/menu.js
@@ -1,64 +1,67 @@
 import { Navbar, Button } from "flowbite-react";
 import HoverLink from './HoverLink';
-import { useState, useEffect } from "react";
 
 export default function Menu() {
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
 
     return (
-        <div className={`${
-            isMobileMenuOpen ? "bg-gradient-to-b from-blue-900 to-blue-900" : "bg-transparent"
-        } md:bg-transparent`}>
+        <div className="bg-gradient-to-b from-blue-900 to-blue-900 md:bg-transparent">
             {/* Menu START */}
             <Navbar
                 fluid={true}
                 rounded={true}
                 style={{ margin: "auto" }}
-                className={`px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-slate-900 ${
-                    isMobileMenuOpen ? "bg-gradient-to-b from-blue-900 to-slate-900" : "bg-transparent"
-                }`}
+                className="px-2 sm:px-4 py-2.5 bg-gradient-to-b  from-blue-900 to-slate-900 fixed w-full z-20 top-0 left-0 border-b  border-slate-900  "
             >
                 <Navbar.Brand href="https://ekehi.network">
                     <img src="/images/logo.png" className="mr-2 sm:h-9" alt="Ekehi Presale" />
                 </Navbar.Brand>
                 <div className="flex md:order-2">
-                    <Navbar.Toggle
-                        className="tcolor order-2"
-                        onClick={() => setIsMobileMenuOpen((prev) => !prev)}
-                    />
+                    <Navbar.Toggle className="tcolor order-2" />
+                    {/* <a href="#presale">
+                        <Button
+                            className="text-white order-1 focus:ring-4 focus:outline-none focus:ring-red-300 rounded-lg font-medium text-sm text-center mr-3 md:mr-0 default-color ">
+                            JOIN PRESALE
+                        </Button>
+                    </a> */}
                 </div>
                 <Navbar.Collapse
-                    className={`flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ${
-                        isMobileMenuOpen ? "bg-gradient-to-b from-slate-900 to-blue-900" : "lg:bg-transparent"
-                    }`}
-                >
-                    <HoverLink href="#home" initialState={true}>
+                    className="flex flex-col p-4 mt-4 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 border-yellow-500 bg-gradient-to-b from-slate-900 to-blue-900 lg:bg-transparent">
+                    <HoverLink href="#home"
+                        initialState={true}>
                         Home
                     </HoverLink>
-                    <HoverLink href="#about" initialState={true}>
+                    <HoverLink href="#about"
+                        initialState={true}>
                         About
                     </HoverLink>
-                    <HoverLink href="#whitepaper" initialState={true}>
+                    <HoverLink href="#whitepaper"
+                        initialState={true}>
                         Whitepaper
                     </HoverLink>
-                    <HoverLink href="#presale" initialState={true}>
+                    <HoverLink href="#presale"
+                        initialState={true}>
                         Presale
                     </HoverLink>
-                    <HoverLink href="#ecosystem" initialState={true}>
+                    <HoverLink href="#ecosystem"
+                        initialState={true}>
                         Ecosystem
                     </HoverLink>
-                    <HoverLink href="#tokenomics" initialState={true}>
+                    <HoverLink href="#tokenomics"
+                        initialState={true}>
                         Tokenomics
                     </HoverLink>
-                    <HoverLink href="#roadmap" initialState={true}>
+                    <HoverLink href="#roadmap"
+                        initialState={true}>
                         Roadmap
                     </HoverLink>
-                    <HoverLink href="#contact" initialState={true}>
+                    <HoverLink href="#contact"
+                        initialState={true}>
                         Contact
                     </HoverLink>
                 </Navbar.Collapse>
             </Navbar>
             {/* Menu END */}
         </div>
-    );
+    )
 }
