b163534cd318f348fade9a96fe84af3ee0d844ff
Ekehi <160106638+Ekehi@users.noreply.github.com>
2025-01-20 16:41:23 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,9 @@ import Airdrop from "./homepageComponents/airdrop.js";
 export default function Home()
 {
   return (
-    <div className="v_dark w-[100vw]">
+    <div className="v_dark w-[100vw] m-auto p-auto">
       <Menu />
-      <main  className="w-[100vw]">
+      <main  className="w-[100vw] m-auto p-auto">
         <HomeSection />
         <About />
         <Section3 />
